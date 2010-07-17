3f8f1cc0f5a38322f877a55f03e7388cb56e932a
Jerry Richardson <jerry@disruptiveventures.com>
2010-07-17 15:12:22 -0400

    bumped to 0.8.14 - fixed admin_activation

--- lib/captain_oveur/routes.rb
+++ lib/captain_oveur/routes.rb
@@ -11,7 +11,7 @@ module CaptainOveur
     def self.draw(map)
     
       map.namespace :admin do |admin|
-         admin.resources :users, :controller => 'captain_oveur/users'
+         admin.resources :users, :controller => 'captain_oveur/users', :member => { :activate_admin => :put, :deactivate_admin => :put }
       end
 
     end
